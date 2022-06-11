19f0a35741530a377116e8757003c7500e634ec4
HsuHsia <42826043+HsuHsia@users.noreply.github.com>
2022-06-11 13:27:49 +0800

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -1,4 +1,4 @@
-# pip install efficient-apriori #安装apriori包
+pip install efficient-apriori #安装apriori包
 
 from efficient_apriori import apriori
 # 实验使用的数据集
@@ -9,5 +9,5 @@ data = [('牛奶','面包','尿不湿','啤酒','榴莲'),
         ('面包','牛奶','尿不湿','可乐','鸡翅')]
 # 调用apriori
 itemsets, rules = apriori(data, min_support=0.6,  min_confidence=1)
-print(itemsets)
-print(rules)
+# print(itemsets)
+# print(rules)
